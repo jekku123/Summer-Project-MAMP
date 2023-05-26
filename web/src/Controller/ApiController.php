4a7151ca8b7e2d61273c14e62430eceaf1efb68b
markusVJH <markus.heinila@pp.inet.fi>
2023-05-26 15:41:33 +0300

    /api now shows the speakers as well

--- web/src/Controller/ApiController.php
+++ web/src/Controller/ApiController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Conference;
+use App\Entity\Speaker;
 
 #[Route('/api', name: 'api_main')]
 class ApiController extends AbstractController
@@ -20,18 +21,51 @@ class ApiController extends AbstractController
     }
 
     #[Route('/', name: 'getAllData', methods: ['GET'])]
-    public function getAllData(): Response
+public function getAllData(): Response
+{
+    $conferences = $this->doctrine->getRepository(Conference::class)->findAll();
+    $speakers = $this->doctrine->getRepository(Speaker::class)->findAll();
+    
+    $data = [
+        'conferences' => [],
+        'speakers' => [],
+    ];
+    foreach ($conferences as $conference) {
+        $data['conferences'][] = [
+            'id' => $conference->getId(),
+            'title' => $conference->getTitle(),
+            'description' => $conference->getDescription(),
+            'location' => $conference->getLocation(),
+            'start_at' => $conference->getStartAt(),
+            'end_at' => $conference->getEndAt(),
+        ];
+    }
+    foreach ($speakers as $speaker) {
+        $data['speakers'][] = [
+            'id' => $speaker->getId(),
+            'firstname' => $speaker->getFirstname(),
+            'lastname' => $speaker->getLastname(),
+            'bio' => $speaker->getBio(),
+            'organization' => $speaker->getOrganization(),
+        ];
+    }
+    return $this->json($data);
+}
+    #[Route('/conference', name: 'getAllConferences', methods: ['GET'])]
+    public function getAllConferences(): Response
     {
         $conferences = $this->doctrine->getRepository(Conference::class)->findAll();
         
         $data = [];
         foreach ($conferences as $conference) {
-            $data[] = [
-                'id' => $conference->getId(),
-                'title' => $conference->getTitle(),
-                'description' => $conference->getDescription(),
-                'location' => $conference->getlocation(),
-            ];
+          $data['conferences'][] = [
+            'id' => $conference->getId(),
+            'title' => $conference->getTitle(),
+            'description' => $conference->getDescription(),
+            'location' => $conference->getLocation(),
+            'start_at' => $conference->getStartAt(),
+            'end_at' => $conference->getEndAt(),
+        ];
         }
         
         return $this->json($data);
