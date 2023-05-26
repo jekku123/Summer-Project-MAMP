49e18d5de7e2f21f5b6075280dbde69050c3c275
markusVJH <markus.heinila@pp.inet.fi>
2023-05-26 14:47:42 +0300

    conference data now accessible at /api

--- web/src/Controller/ApiController.php
+++ web/src/Controller/ApiController.php
@@ -6,11 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Conference;
 
 #[Route('/api', name: 'api_main')]
-class ProjectController extends AbstractController
+class ApiController extends AbstractController
 {
-  
+    private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    #[Route('/', name: 'getAllData', methods: ['GET'])]
+    public function getAllData(): Response
+    {
+        $conferences = $this->doctrine->getRepository(Conference::class)->findAll();
+        
+        $data = [];
+        foreach ($conferences as $conference) {
+            $data[] = [
+                'id' => $conference->getId(),
+                'title' => $conference->getTitle(),
+                'description' => $conference->getDescription(),
+                'location' => $conference->getlocation(),
+            ];
+        }
+        
+        return $this->json($data);
+    }
 }
