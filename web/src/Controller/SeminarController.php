4a8127b69b608ef61a1176b5c36d779bce88bb9c
markusVJH <markus.heinila@pp.inet.fi>
2023-05-29 13:19:50 +0300

    /seminars getting speakers and sessions now

--- web/src/Controller/SeminarController.php
+++ web/src/Controller/SeminarController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Seminar;
+use App\Entity\Session;
 use App\Repository\SeminarRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -31,10 +32,87 @@ class SeminarController extends AbstractController
               'location' => $seminar->getLocation(),
               'image' => $seminar->getImage(),
               'start_at' => $seminar->getStartAt()->format('Y-m-d H:i:s'),
-              'end_at' => $seminar->getEndAt()->format('Y-m-d H:i:s')
+              'end_at' => $seminar->getEndAt()->format('Y-m-d H:i:s'),
+              'speakers' => $this->getSpeakersBySeminar($seminar),
+              'sessions' => $this->getSessionsBySeminar($seminar),
           ];
       }
 
       return $this->json($response);
   }
+
+  #[Route('/seminars/{id}', name: 'api_one', methods: ['GET'])]
+    public function getOneConfrence($id): JsonResponse
+    {
+        $seminar = $this->seminars->find($id);
+
+        if (!$seminar) {
+            return $this->json('Nothing found with id: ' . $id, 400);
+        }
+
+        $response[] = [
+            'id' => $seminar->getId(),
+            'title' => $seminar->getTitle(),
+            'description' => $seminar->getDescription(),
+            'location' => $seminar->getLocation(),
+            'image' => $seminar->getImage(),
+            'start_at' => $seminar->getStartAt()->format('Y-m-d H:i:s'),
+            'end_at' => $seminar->getEndAt()->format('Y-m-d H:i:s'),
+            'speakers' => $this->getSpeakersBySeminar($seminar),
+            'sessions' => $this->getSessionsByseminar($seminar)
+        ];
+
+        return $this->json($response);
+    }
+    private function getSessionsBySeminar(Seminar $seminar)
+    {
+        $sessions = [];
+
+        foreach ($seminar->getSessions() as $session) {
+            $sessions[] = [
+                'id' => $session->getId(),
+                'title' => $session->getTitle(),
+                'description' => $session->getDescription(),
+                'location' => $session->getLocation(),
+                'start_at' => $session->getStartAt()->format('Y-m-d H:i:s'),
+                'end_at' => $session->getEndAt()->format('Y-m-d H:i:s'),
+                'speakers' => $this->getSpeakersBySession($session),
+            ];
+        }
+
+        return $sessions;
+    }
+    private function getSpeakersBySeminar(Seminar $seminar)
+    {
+        $speakers = [];
+
+        foreach ($seminar->getSpeakers() as $speaker) {
+            $speakers[] = [
+                'id' => $speaker->getId(),
+                'firstname' => $speaker->getFirstname(),
+                'lastname' => $speaker->getLastname(),
+                'bio' => $speaker->getBio(),
+                'organization' => $speaker->getOrganization(),
+            ];
+        }
+
+        return $speakers;
+    }
+    private function getSpeakersBySession(Session $session)
+    {
+        $speakers = [];
+
+        foreach ($session->getSessionSpeakers() as $sessionSpeaker) {
+            $speaker = $sessionSpeaker->getSpeaker();
+            $speakers[] = [
+                'id' => $speaker->getId(),
+                'firstname' => $speaker->getFirstname(),
+                'lastname' => $speaker->getLastname(),
+                'bio' => $speaker->getBio(),
+                'organization' => $speaker->getOrganization(),
+            ];
+        }
+
+        return $speakers;
+    }
 }
