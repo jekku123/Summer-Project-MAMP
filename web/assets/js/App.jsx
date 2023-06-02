7469807f189591e59a980d578d2587ccd31be846
Jesse Manninen <jesse.manninen@gmail.com>
2023-06-02 21:00:31 +0300

    put scroll top while changing page effect to App

--- web/assets/js/App.jsx
+++ web/assets/js/App.jsx
@@ -1,36 +1,32 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import React, { useEffect } from "react";
-import Home from "./pages/Home/Home";
-import Layout from "./components/UI/Layout";
-import Conference from "./pages/Event/Conference";
-import Helsinki from "./pages/Helsinki/Helsinki";
-import FaqInfo from "./pages/Faq/FaqInfo";
-import EventList from "./pages/EventList/EventList";
-import "./App.css";
-import ScrollTopBtn from "./components/ScrollTopBtn/ScrollTopBtn";
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import Home from './pages/Home/Home';
+import Layout from './components/UI/Layout';
+import Conference from './pages/Event/Conference';
+import Helsinki from './pages/Helsinki/Helsinki';
+import FaqInfo from './pages/Faq/FaqInfo';
+import EventList from './pages/EventList/EventList';
+import './App.css';
+import ScrollTopBtn from './components/ScrollTopBtn/ScrollTopBtn';
 
-function ScrollToTop() {
+function App() {
     const { pathname } = useLocation();
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [pathname]);
 
-    return null;
-}
-
-function App() {
     return (
-        <div className="App">
+        <div className='App'>
             <BrowserRouter>
                 <ScrollToTop />
                 <Routes>
-                    <Route path="/" element={<Layout />}>
+                    <Route path='/' element={<Layout />}>
                         <Route index element={<Home />} />
-                        <Route path="/helsinki" element={<Helsinki />} />
-                        <Route path="/events" element={<EventList />} />
-                        <Route path="/conference" element={<Conference />} />
-                        <Route path="/faq" element={<FaqInfo />} />
+                        <Route path='/helsinki' element={<Helsinki />} />
+                        <Route path='/events' element={<EventList />} />
+                        <Route path='/conference' element={<Conference />} />
+                        <Route path='/faq' element={<FaqInfo />} />
                     </Route>
                 </Routes>
                 <ScrollTopBtn />
