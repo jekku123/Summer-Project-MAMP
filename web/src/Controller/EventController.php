80ecc4387612666045225df7a4ae49d1680ace59
Jesse Manninen <jesse.manninen@gmail.com>
2023-06-06 06:42:35 +0300

    sms notifier

--- web/src/Controller/EventController.php
+++ web/src/Controller/EventController.php
@@ -11,12 +11,15 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Notifier\Message\SmsMessage;
+use Symfony\Component\Notifier\TexterInterface;
 use App\Mailer\Mailer;
 use Doctrine\ORM\EntityManagerInterface;
 
-#[Route('/api', name: 'api_')          ]
+#[Route('/api', name: 'api_')]
 class EventController extends AbstractController
 {
+
     #[Route('/events', name: 'get_events', methods: ['GET'])]
     public function getAll(EntityManagerInterface $entityManager): Response
     {
