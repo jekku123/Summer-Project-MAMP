89983963e29c5b6ed07bd2eb50a0fe19af1936e8
Jesse Manninen <jesse.manninen@gmail.com>
2023-06-08 02:31:18 +0300

    made signup form funny

--- web/assets/js/pages/SignUp/SignUp.jsx
+++ web/assets/js/pages/SignUp/SignUp.jsx
@@ -1,88 +1,51 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router';
+import React from 'react';
+import { useNavigate, useParams } from 'react-router';
 import axios from 'axios';
-import './SignUp.css';
 import useAxios from '../../hooks/useAxios';
+import useForm from '../../hooks/useForm';
+import TextField from './components/TextField';
+import alertBox from './utils/alertBox';
+import './SignUp.css';
+
+const formTemplate = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    phone: '',
+};
 
 const SignUp = () => {
-    const [formData, setFormData] = useState({
-        firstname: '',
-        lastname: '',
-        email: '',
-        phone: '',
-    });
     const params = useParams();
-    const [data] = useAxios(`http://localhost:8007/api/events/${params.id}`);
+    const navigate = useNavigate();
+    const [event] = useAxios(`http://localhost:8007/api/events/${params.id}`);
+    const { formData, handleFormChanges, handleSubmit } = useForm(formTemplate);
 
-    const handleChanges = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevState) => ({
-            ...prevState,
-            [name]: value,
-        }));
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const submit = async () => {
         const postData = { ...formData, event_id: params.id };
-        axios
-            .post(`http://localhost:8007/api/attendee`, postData)
-            .then(() => {
-                alert(`Thank you for signing up to ${data.title}`);
-                setFormData({
-                    firstname: '',
-                    lastname: '',
-                    email: '',
-                    phone: '',
-                });
-            })
-            .catch((err) => alert('Error! just couldnt :('));
+        try {
+            await axios.post(`http://localhost:8007/api/attendee`, postData);
+            alertBox(event, true);
+            navigate('/');
+        } catch (error) {
+            alertBox(null, false);
+        }
     };
 
     return (
         <div className='signup'>
             <h2>Sign up for the event</h2>
-            <form onSubmit={handleSubmit}>
-                <div className='textfield'>
-                    <label htmlFor='firstname'>Firstname</label>
-                    <input
-                        name='firstname'
-                        id='firstname'
-                        onChange={handleChanges}
-                        value={formData.firstname}
-                    />
-                </div>
-
-                <div className='textfield'>
-                    <label htmlFor='lastname'>Lastname</label>
-                    <input
-                        name='lastname'
-                        id='lastname'
-                        onChange={handleChanges}
-                        value={formData.lastname}
-                    />
-                </div>
-                <div className='textfield'>
-                    <label htmlFor='email'>Email</label>
-                    <input
-                        name='email'
-                        id='email'
-                        onChange={handleChanges}
-                        value={formData.email}
-                    />
-                </div>
-                <div className='textfield'>
-                    <label htmlFor='phone'>Phone</label>
-                    <input
-                        name='phone'
-                        id='phone'
-                        onChange={handleChanges}
-                        value={formData.phone}
-                    />
-                </div>
-                <div>
-                    <button type='submit'>Sign up</button>
-                </div>
+            <form onSubmit={(e) => handleSubmit(e, submit)}>
+                {Object.keys(formData).map((fieldname, i) => (
+                    <TextField
+                        key={i}
+                        name={fieldname}
+                        value={formData[fieldname]}
+                        onChange={handleFormChanges}
+                    >
+                        {fieldname}
+                    </TextField>
+                ))}
+                <button type='submit'>Sign up</button>
             </form>
         </div>
     );
--- web/src/Mailer/Mailer.php
+++ web/src/Mailer/Mailer.php
@@ -21,11 +21,13 @@ class Mailer
         $email = (new Email())
             ->from('business.college@bc.fi')
             ->to($attendee->getEmail())
-            ->subject('Welcome to our event ' . $attendee->getFirstname() . '!')
+            ->subject('Registration confirmation for ' . $attendee->getEvent()->getTitle())
             ->text('Super awesome')
             ->html('
-            <h1>Thank you for registering!</h1>
-            <p>Lets have a good one!</p>
+            <h1>Hey ' . $attendee->getFirstname() . '!</h1>
+            <p>Thank you for registering!</p>
+            <p>Lets have a good one! :)</p>
+            <br>
             <small>-Business College-</small>
             ');
 
@@ -41,10 +43,14 @@ class Mailer
             ->subject('Invite to: ' . $invite->getEvent()->getTitle())
             ->text('Please come')
             ->html('
-            <h1>Please come to our ' . $invite->getEvent()->getType() . '</h1>
-            <p>Lets have a good one!</p>
-            <p>' . $invite->getEvent()->getStartAt()->format('Y-m-d H:i:s') . ' @ ' . $invite->getEvent()->getLocation() . '</p>
-            <a href="http://localhost:8007/signup/' . $id . '">Sign up</a>
+            <h1>Hey!</h1> 
+            <p>We would like to invite you to our ' . $invite->getEvent()->getType() . ': ' . $invite->getEvent()->getTitle() . '</p>
+            <p>Please fill the click the link at the bottom and sign up if you are gonna attend! :)</p>
+            <p>Where: <span>' . $invite->getEvent()->getLocation() . '</span></ul>
+            <p>When: <span>' . $invite->getEvent()->getStartAt()->format('d-m-Y H:i:') . '</span></p>
+            <br>
+            <a href="http://localhost:8007/signup/' . $id . '">Sign up here</a>
+            <br><br>
             <small>-Business College-</small>
             ');
 
