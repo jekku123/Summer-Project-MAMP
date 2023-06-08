07217c73e27731d454c3e611d77204ffb041e2f3
markusVJH <markus.heinila@pp.inet.fi>
2023-06-08 12:37:43 +0300

    arringing dashboard and making plural titles

--- web/src/Controller/Admin/SideEventCrudController.php
+++ web/src/Controller/Admin/SideEventCrudController.php
@@ -8,12 +8,19 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class SideEventCrudController extends AbstractCrudController
 {
   public static function getEntityFqcn(): string
   {
     return SideEvent::class;
+  }
+  public function configureCrud(Crud $crud): Crud
+  {
+    return $crud
+        ->setEntityLabelInSingular('Side event')
+        ->setEntityLabelInPlural('Side events');
   }
 
   public function configureFields(string $pageName): iterable
