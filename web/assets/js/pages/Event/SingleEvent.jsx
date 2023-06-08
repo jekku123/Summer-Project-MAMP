b04b112eb70058f1e6143eaa91d16f2d4009855a
shree0007 <shree.bhusal07@gmail.com>
2023-06-08 11:06:20 +0300

    updated single page

--- web/assets/js/pages/Event/SingleEvent.jsx
+++ web/assets/js/pages/Event/SingleEvent.jsx
@@ -16,11 +16,11 @@ function SingleEvent() {
         </div>
     ) : (
         <div className='event'>
-            <h1 className='title'>About the Event</h1>
+            <h1 className='title'>About {data.type}</h1>
 
             <div className='section1'>
                 <div className='section1-main'>
-                    <h2 className='section-title'>Event Information</h2>
+                    <h2 className='section-title'>{data.type} Information</h2>
                     <div className='section-content'>
                         <p>
                             <strong>Title:</strong> {data.title}
@@ -46,20 +46,23 @@ function SingleEvent() {
                 />
             </div>
 
-            <div className='section'>
-                <h2 className='section-title'>Speakers</h2>
-                <div className='section-content'>
-                    {data.speakers &&
-                        data.speakers.map((speaker, i) => (
-                            <div className='speaker' key={speaker.firstname}>
-                                <p>
-                                    {speaker.firstname}
-                                    {speaker.lastname}
-                                </p>
-                            </div>
-                        ))}
+            {data.speakers && data.speakers.length > 0 ? (
+                <div className='section'>
+                    <h2 className='section-title'>Speakers</h2>
+                    <div className='section-content-speakers'>
+                        {data.speakers &&
+                            data.speakers.map((speaker, i) => (
+                                <div className='speaker' key={speaker.firstname}>
+                                    <p>
+                                        {speaker.firstname}
+                                        {speaker.lastname}
+                                    </p>
+                                </div>
+                            ))}
+                    </div>
                 </div>
-            </div>
+            ) : null}
+
 
             <div className='section'>
                 <h2 className='section-title'>Sessions</h2>
@@ -100,133 +103,142 @@ function SingleEvent() {
                 </div>
             </div>
 
-            <div className='section'>
-                <h2 className='section-title'>Exhibition</h2>
-                <div className='section-content'>
-                    {data.exhibitions &&
-                        data.exhibitions.map((exhibition, i) => (
-                            <div className='exhibition' key={exhibition.title}>
-                                <div className='exhibition-info'>
-                                    <p>
-                                        <strong>Title: </strong>
-                                        {exhibition.title}
-                                    </p>
-                                    <p>
-                                        <strong>Description: </strong>
-                                        {exhibition.description}
-                                    </p>
-                                    <p>
-                                        <strong>Location: </strong>
-                                        {exhibition.location}
-                                    </p>
-                                    <p>
-                                        <strong>Start Date/Time: </strong>{' '}
-                                        {exhibition.start_at}
-                                    </p>
-                                    <p>
-                                        <strong>End Date/Time: </strong>
-                                        {exhibition.end_at}
-                                    </p>
-                                </div>
-                                <p>
-                                    {' '}
-                                    <strong>Booths:</strong>
-                                </p>
-                                {exhibition.booths.map((booth) => (
-                                    <div
-                                        className='exhibition-booth'
-                                        key={booth.booth_number}
-                                    >
+            {data.exhibitions && data.exhibitions.length > 0 ? (
+                <div className='section'>
+                    <h2 className='section-title'>Exhibition</h2>
+                    <div className='section-content'>
+                        {data.exhibitions &&
+                            data.exhibitions.map((exhibition, i) => (
+                                <div className='exhibition' key={exhibition.title}>
+                                    <div className='exhibition-info'>
                                         <p>
-                                            <strong>Booth number: </strong>{' '}
-                                            {booth.booth_number}
+                                            <strong>Title: </strong>
+                                            {exhibition.title}
                                         </p>
                                         <p>
-                                            <strong>Title: </strong>{' '}
-                                            {booth.title}
+                                            <strong>Description: </strong>
+                                            {exhibition.description}
                                         </p>
                                         <p>
-                                            <strong>Description: </strong>{' '}
-                                            {booth.description}
-                                        </p>
-
-                                        <p className='booth-company'>
-                                            {' '}
-                                            <strong>Company: </strong>
-                                            <a href='https://www.neste.com/'>
-                                                {booth.company.name}
-                                            </a>
+                                            <strong>Location: </strong>
+                                            {exhibition.location}
                                         </p>
                                         <p>
-                                            {' '}
-                                            <strong>About company: </strong>
-                                            {booth.company.description}
+                                            <strong>Start Date/Time: </strong>{' '}
+                                            {exhibition.start_at}
+                                        </p>
+                                        <p>
+                                            <strong>End Date/Time: </strong>
+                                            {exhibition.end_at}
                                         </p>
                                     </div>
-                                ))}
-                            </div>
-                        ))}
-                </div>
-            </div>
+                                    <p>
+                                        {' '}
+                                        <strong>Booths:</strong>
+                                    </p>
+                                    {exhibition.booths.map((booth) => (
+                                        <div
+                                            className='exhibition-booth'
+                                            key={booth.booth_number}
+                                        >
+                                            <p>
+                                                <strong>Booth number: </strong>{' '}
+                                                {booth.booth_number}
+                                            </p>
+                                            <p>
+                                                <strong>Title: </strong>{' '}
+                                                {booth.title}
+                                            </p>
+                                            <p>
+                                                <strong>Description: </strong>{' '}
+                                                {booth.description}
+                                            </p>
 
-            <div className='section'>
-                <h2 className='section-title'>Workshops</h2>
-                <div className='section-content'>
-                    {data.workshops &&
-                        data.workshops.map((workshop, i) => (
-                            <div className='workshop' key={workshop.title}>
-                                <p>
-                                    <strong>Title:</strong>
-                                    {workshop.title}
-                                </p>
-                                <p>
-                                    <strong>Description:</strong>
-                                    {workshop.description}
-                                </p>
-                                <p>
-                                    <strong>Location:</strong>
-                                    {workshop.location}
-                                </p>
-                                <p>
-                                    <strong>Start Date/Time:</strong>{' '}
-                                    {workshop.start_at}
-                                </p>
-                                <p>
-                                    <strong>End Date/Time:</strong>{' '}
-                                    {workshop.end_at}
-                                </p>
-                                <p>
-                                    {' '}
-                                    <strong>Speakers:</strong>
-                                    {workshop.speakers.map((speaker) => (
-                                        <li key={speaker.firstname}>
-                                            {speaker.firstname}
-                                            {speaker.lastname}
-                                        </li>
+                                            <p className='booth-company'>
+                                                {' '}
+                                                <strong>Company: </strong>
+                                                <a href='https://www.neste.com/'>
+                                                    {booth.company.name}
+                                                </a>
+                                            </p>
+                                            <p>
+                                                {' '}
+                                                <strong>About company: </strong>
+                                                {booth.company.description}
+                                            </p>
+                                        </div>
                                     ))}
-                                </p>
+                                </div>
+                            ))}
+                    </div>
+                </div>
+            ) : null}
+
+            {data.workshops && data.workshops.length > 0 ? (
+                <div className='section'>
+                    <h2 className='section-title'>Workshops</h2>
+                    <div className='section-content'>
+                        {data.workshops &&
+                            data.workshops.map((workshop, i) => (
+                                <div className='workshop' key={workshop.title}>
+                                    <p>
+                                        <strong>Title:</strong>
+                                        {workshop.title}
+                                    </p>
+                                    <p>
+                                        <strong>Description:</strong>
+                                        {workshop.description}
+                                    </p>
+                                    <p>
+                                        <strong>Location:</strong>
+                                        {workshop.location}
+                                    </p>
+                                    <p>
+                                        <strong>Start Date/Time:</strong>{' '}
+                                        {workshop.start_at}
+                                    </p>
+                                    <p>
+                                        <strong>End Date/Time:</strong>{' '}
+                                        {workshop.end_at}
+                                    </p>
+                                    <p>
+                                        {' '}
+                                        <strong>Speakers:</strong>
+                                        {workshop.speakers.map((speaker) => (
+                                            <li key={speaker.firstname}>
+                                                {speaker.firstname}
+                                                {speaker.lastname}
+                                            </li>
+                                        ))}
+                                    </p>
+                                </div>
+                            ))}
+                    </div>
+                </div>
+            ) : null}
+
+            {data.sideEvents && data.sideEvents.length > 0 ? (
+                <div className='section'>
+                    <h2 className='section-title'>Side Events</h2>
+                    {data.sideEvents &&
+                        data.sideEvents.map((sideEvent, i) => (
+                            <div className='section-content'>
+                                <div className='side-event' key={sideEvent.title}>
+                                    <p>title: {sideEvent.title}</p>
+                                    <p>description: {sideEvent.description}</p>
+                                    <p>Location: {sideEvent.location}</p>
+                                    <p>Start Time: {sideEvent.start_at}</p>
+                                    <p>End Time: {sideEvent.end_at}</p>
+                                </div>
                             </div>
                         ))}
                 </div>
-            </div>
+            ) : null}
 
-            <div className='section'>
-                <h2 className='section-title'>Side Events</h2>
-                {data.sideEvents &&
-                    data.sideEvents.map((sideEvent, i) => (
-                        <div className='section-content'>
-                            <div className='side-event' key={sideEvent.title}>
-                                <p>title: {sideEvent.title}</p>
-                                <p>description: {sideEvent.description}</p>
-                                <p>Location: {sideEvent.location}</p>
-                                <p>Start Time: {sideEvent.start_at}</p>
-                                <p>End Time: {sideEvent.end_at}</p>
-                            </div>
-                        </div>
-                    ))}
-            </div>
         </div>
     );
 }
 
 export default SingleEvent;
+
+
